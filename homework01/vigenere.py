3c4c8678337ae1ee8545c2afea610b099a1bd10d
ViktoriiaBaban-K3140 <viktoriya.baban.03@mail.ru>
2020-10-07 18:08:35 +0300

    Реализованы обе функции

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -1,3 +1,6 @@
+m = 'abcdefghijklmnopqrstuvwxyz'
+b = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
 def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     """
     Encrypts plaintext using a Vigenere cipher.
@@ -10,7 +13,25 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     'LXFOPVEFRNHR'
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    space = 0
+    for index, x in enumerate(plaintext):
+        if x != ' ':
+            t = b.find(x)
+            if t == -1:
+              t = m.find(x)
+              if t == -1:
+                ciphertext += x
+              else:
+                k = m.find(keyword[index % len(keyword)])
+                r = (t + k) % len(m)
+                ciphertext += m[r]
+            else:
+              k = b.find(keyword[index % len(keyword)])
+              r = (t + k) % len(b)
+              ciphertext += b[r]
+        else:
+            space +=1
+            ciphertext += ' ' 
     return ciphertext
 
 
@@ -26,5 +47,23 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     'ATTACKATDAWN'
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    space = 0
+    for index, x in enumerate(ciphertext):
+        if x != ' ':
+            t = b.find(x)
+            if t == -1:
+              t = m.find(x)
+              if t == -1:
+                plaintext += x
+              else:
+                k = m.find(keyword[index % len(keyword)])
+                r = (t - k) % len(m)
+                plaintext += m[r]
+            else:
+              k = b.find(keyword[index % len(keyword)])
+              r = (t - k) % len(b)
+              plaintext += b[r]
+        else:
+            space +=1
+            plaintext += ' ' 
     return plaintext
