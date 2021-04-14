ad2fb544db57ad933fa339f87b1bb0be3ec226f4
Виктория Бабан <71071574+ViktoriiaBaban-K3140@users.noreply.github.com>
2021-04-14 19:25:43 +0300

    changes import

---
+++ homework06/test_scraputils.py
@@ -0,0 +1,34 @@
+from scraputils import *
+
+
+def test_extract_next_page() -> None:
+    url = "https://news.ycombinator.com/"
+    for i in range(2, 5):
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, "html.parser")
+        next_url = extract_next_page(soup)
+        assert next_url == "news?p=" + str(i)
+        url = "https://news.ycombinator.com/" + next_url
+
+
+def test_extract_news() -> None:
+    url = "https://news.ycombinator.com/"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    current_news = extract_news(soup)
+    news_keys = {"author", "points", "title", "url"}
+    assert set(current_news[0].keys()) == news_keys
+
+
+def test_get_news() -> None:
+    news = []
+    url = "https://news.ycombinator.com/"
+
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    current_news = extract_news(soup)
+    next_url = extract_next_page(soup)
+    url = "https://news.ycombinator.com/" + next_url
+    news.extend(current_news)
+
+    assert get_news("https://news.ycombinator.com/") == news
