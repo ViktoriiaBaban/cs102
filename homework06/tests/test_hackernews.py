8efd712b91ff7fdd5b84d7b6ed70b1a7e04f7dc8
Виктория Бабан <71071574+ViktoriiaBaban-K3140@users.noreply.github.com>
2021-04-14 18:44:43 +0300

    Add files via upload

---
+++ homework06/tests/test_hackernews.py
@@ -0,0 +1,8 @@
+import hackernews
+
+
+def test_clear() -> None:
+    assert hackernews.clean("MOLLY") == "MOLLY"
+    assert hackernews.clean("MOl-ly.") == "MOlly"
+    assert hackernews.clean("M, O, L, L, Y") == "M O L L Y"
+
