09611b3e08f03f1b6bbc5796ef970532d17f7a9f
Виктория Бабан <71071574+ViktoriiaBaban-K3140@users.noreply.github.com>
2021-05-18 20:36:33 +0300

    Add homework07

---
+++ homework07-web/httpserver/httpserver/handlers.py
@@ -0,0 +1,122 @@
+from __future__ import annotations
+
+import socket
+import typing as tp
+
+from httptools import HttpRequestParser
+from httptools.parser.errors import (
+    HttpParserCallbackError,
+    HttpParserError,
+    HttpParserInvalidMethodError,
+    HttpParserInvalidStatusError,
+    HttpParserInvalidURLError,
+    HttpParserUpgrade,
+)
+
+from .request import HTTPRequest
+from .response import HTTPResponse
+
+if tp.TYPE_CHECKING:
+    from .server import TCPServer
+
+Address = tp.Tuple[str, int]
+
+
+class BaseRequestHandler:
+    def __init__(self, socket: socket.socket, address: Address, server: TCPServer) -> None:
+        self.socket = socket
+        self.address = address
+        self.server = server
+
+    def handle(self) -> None:
+        self.close()
+
+    def close(self) -> None:
+        self.socket.close()
+
+
+class EchoRequestHandler(BaseRequestHandler):
+    def handle(self) -> None:
+        try:
+            data = self.socket.recv(1024)
+        except (socket.timeout, BlockingIOError):
+            pass
+        else:
+            self.socket.sendall(data)
+        finally:
+            self.close()
+
+
+class BaseHTTPRequestHandler(BaseRequestHandler):
+    request_klass = HTTPRequest
+    response_klass = HTTPResponse
+
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.parser = HttpRequestParser(self)
+
+        self._url: bytes = b""
+        self._headers: tp.Dict[bytes, bytes] = {}
+        self._body: bytes = b""
+        self._parsed = False
+
+    def handle(self) -> None:
+        request = self.parse_request()
+        if request:
+            try:
+                response = self.handle_request(request)
+            except Exception:
+                print("Ooops... Error 500")
+                response = self.response_klass(status=500, headers={}, body=b"")
+        else:
+            response = self.response_klass(status=400, headers={}, body=b"")
+        self.handle_response(response)
+        self.close()
+
+    def parse_request(self) -> tp.Optional[HTTPRequest]:
+        while not self._parsed:
+            try:
+                data = self.socket.recv(1024)
+                if data == b"":
+                    print("Ok. Parsed done successfully")
+                    break
+                self.parser.feed_data(data)
+            except socket.timeout:
+                print("Ooops... Timeout!")
+                break
+            except (
+                HttpParserError,
+                HttpParserCallbackError,
+                HttpParserInvalidStatusError,
+                HttpParserInvalidMethodError,
+                HttpParserInvalidURLError,
+                HttpParserUpgrade,
+            ):
+                print("Ooops... Parser error")
+                break
+        if self._parsed:
+            return self.request_klass(
+                method=self.parser.get_method(),
+                url=self._url,
+                headers=self._headers,
+                body=self._body,
+            )
+        return None
+
+    def handle_request(self, request: HTTPRequest) -> HTTPResponse:
+        return self.response_klass(status=405, headers={}, body=b"")
+
+    def handle_response(self, response: HTTPResponse) -> None:
+        self.socket.sendall(response.to_http1())
+
+    def on_url(self, url: bytes) -> None:
+        self._url = url
+
+    def on_header(self, name: bytes, value: bytes) -> None:
+        self._headers[name] = value
+
+    def on_body(self, body: bytes) -> None:
+        self._body = body
+
+    def on_message_complete(self) -> None:
+        self._parsed = True
---
+++ homework07-web/httpserver/httpserver/response.py
@@ -0,0 +1,23 @@
+import dataclasses
+import http.client
+import typing as tp
+
+
+@dataclasses.dataclass
+class HTTPResponse:
+    status: int
+    headers: tp.Dict[str, str] = dataclasses.field(default_factory=dict)
+    body: bytes = b""
+
+    def to_http1(self) -> bytes:
+        http1 = (
+            f"HTTP/1.1 {self.status} {http.client.responses[self.status]}\r\n"
+            + "\r\n".join(
+                [
+                    f"{key}: {value}"
+                    for key, value in zip(self.headers.keys(), self.headers.values())
+                ]
+            )
+            + f"\r\n\r\n{self.body.decode()}"
+        )
+        return http1.encode()
